d49d2d7b2817cf1f321f3ece712479d1a8bdb529
MartinBrerat <93645326+MartinBrerat@users.noreply.github.com>
2021-12-10 10:25:00 +0100

    Add files via upload

---
+++ JV1A_BRERATMARTIN_Motus.py
@@ -0,0 +1,30 @@
+import random
+
+def motCacher(mot):
+    print("choisir une lettre")
+    motChercher=int(input())
+    lettreChoisie=()
+    
+    for i in range(len(mot)):        
+        if(mot[i]=="T" or mot[i]=="O" or mot[i]=="T" or mot[i]=="O" or mot[i]=="R" or mot[i]=="O"):
+
+        else  ([mot] = motchercher):
+
+def victoire(test):
+    test= motChercher
+    for i in range(len(mot)):
+        if(test[i] == "T" + test[i+1] == "O" + test[i+2] == "T" + test[i+3] == "O" + test[i+4] == "R" + test[i+5] == "O"):
+            print("victoir")
+
+def defaite(test):
+    tour=0
+    for i in range(0,6):
+        tour[i+1]
+    if tour == 6:
+        else (print("defaite")):
+
+#debut prog principal
+
+motMystere = "TOTORO"
+print(motCacher(motMystere))
+
